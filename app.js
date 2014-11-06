575827577a4b413d8d55401c11ef2b0b16b9af34
pcruise <TaeYoung@KimTaeYoungui-Mac-mini.local>
2014-11-06 17:24:54 +0900

    server ip (hoob)

--- app.js
+++ app.js
@@ -19,5 +19,4 @@ http.createServer(function (req, res) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(1337, '1.234.27.35');
