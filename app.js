2b7513e4cd22af5376361242e182714d9c1128a3
pcruise <TaeYoung@KimTaeYoungui-Mac-mini.local>
2014-11-06 17:43:15 +0900

    gg. using express

--- app.js
+++ app.js
@@ -6,32 +6,68 @@ webhookToken = "tLK3w8TgCnbeLixtqXqKpqDG";
 
 slack = new Slack(webhookToken, domain);
 
-var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-  if (req.method == 'POST') {
-    var body = '';
-    req.on('data', function (data) {
-      body += data;
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-    req.on('end', function () {
-      util.log("New Response here:");
-      console.log(body);
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('post received');
-  }
+});
 
-  // slack.webhook({
-  //   channel: "#developer",
-  //   username: "slackbot",
-  //   text: "Hello :)"
-  // }, function(err, response) {
-  //   console.log(response);
-  // });
+var debug = require('debug')('peoply-slackbot');
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('');
-}).listen(1337, '1.234.27.35');
-console.log("Server Listening!");
+app.set('port', process.env.PORT || 1337);
+
+var server = app.listen(app.get('port'), function() {
+  util.log('Express server listening on port ' + server.address().port);
+});
